7c8ca0156d49bccb419db6edf99e489544a2bbce
Amal Rizvi <amalrizv@gmail.com>
2020-05-23 23:18:07 +0000

    better stacked bsp plot

--- plotting_and_reporting/bsp_stacked.py
+++ plotting_and_reporting/bsp_stacked.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import cm
 import itertools as it
-from cycler import cycler
 import logging 
 import os
 import sys
@@ -32,6 +32,8 @@ class ExperimentFrame:
     langs       = []
     ops         = []
     proc_counts = []
+    types       = []
+    elms        = 1
 
     # Find which languages, operations, and proc counts
     def discover_params(self):
@@ -43,7 +45,7 @@ class ExperimentFrame:
             if not ext == ".dat":
                 continue
 
-            op, lang, proc_count = basename.split('_')
+            op, lang, proc_count, tp = basename.split('_')
 
             if op not in self.ops:
                 self.ops.append(op)
@@ -51,18 +53,24 @@ class ExperimentFrame:
                 self.langs.append(lang)
             if proc_count not in self.proc_counts:
                 self.proc_counts.append(proc_count)
+            if tp not in self.types:
+                self.types.append(tp)
+
+            self.proc_counts = sorted(self.proc_counts)
+            self.langs = sorted(self.langs)
+            self.ops = sorted(self.ops)
 
 
     # Is this even a valid experiment combination?
-    def check_cfg(self, lang, proc_count):
-        if lang not in self.langs or proc_count not in self.proc_counts:
-            logging.error(f"No such experimental configuration {lang}:{proc_count}")
+    def check_cfg(self, lang, proc_count, tp):
+        if lang not in self.langs or proc_count not in self.proc_counts or tp not in self.types:
+            logging.error(f"No such experimental configuration {lang}:{proc_count}:{tp}")
             return False
         return True
 
-    def check_cfg_with_op(self, lang, proc_count, op):
-        if lang not in self.langs or proc_count not in self.proc_counts or op not in self.ops:
-            logging.error(f"No such experimental configuration {lang}:{proc_count}")
+    def check_cfg_with_op(self, lang, proc_count, op, tp):
+        if lang not in self.langs or proc_count not in self.proc_counts or op not in self.ops or tp not in self.types:
+            logging.error(f"No such experimental configuration {lang}:{proc_count}:{tp}")
             return False
         return True
 
@@ -70,25 +78,28 @@ class ExperimentFrame:
     # Return the mean of all measurements of a particular operation for a
     # particular language/proc_count experiment. This will correspond to the
     # mean of recorded values in one .dat file
-    def op_mean(self, lang, proc_count, op):
-        self.check_cfg_with_op(lang, proc_count, op)
-        rows = self.df.loc[(self.df['op'] == op) & (self.df['lang'] == lang) & (self.df['proc_count'] == proc_count)]
-        return np.mean(rows['measurement'].values)
+    def op_mean(self, lang, proc_count, op, tp):
+        self.check_cfg_with_op(lang, proc_count, op, tp)
+        rows = self.df.loc[(self.df['op'] == op) & (self.df['lang'] == lang) & (self.df['proc_count'] == proc_count) & (self.df['type'] == tp)]
+        if op == 'comms':
+            return np.mean(rows['measurement'].values)
+        else:
+            return np.mean(rows['measurement'].values)*self.elms
 
 
     # Return the number of trials for a given experimental configuration
-    def op_trials(self, lang, proc_count, op):
-        self.check_cfg_with_op(lang, proc_count, op)
-        rows = self.df.loc[(self.df['lang'] == lang) & (self.df['proc_count'] == proc_count) & (self.df['op'] == op)]
+    def op_trials(self, lang, proc_count, op, tp):
+        self.check_cfg_with_op(lang, proc_count, op, tp)
+        rows = self.df.loc[(self.df['lang'] == lang) & (self.df['proc_count'] == proc_count) & (self.df['op'] == op) & (self.df['type'] == tp)]
         return rows.shape[0]
 
 
     # The total of an experiment is calculated as the sum of the means of all ops
     # for that experimental configuration
-    def exp_total(self, lang, proc_count):
+    def exp_total(self, lang, proc_count, tp):
         total = 0
         for op in self.ops:
-            total += self.op_mean(lang, proc_count, op)
+            total += self.op_mean(lang, proc_count, op, tp)
         return total
 
 
@@ -101,6 +112,7 @@ class ExperimentFrame:
         logging.debug("Languages: {}".format(self.langs))
         logging.debug("Operations: {}".format(self.ops))
         logging.debug("Process Counts: {}".format(self.proc_counts))
+        logging.debug("Types: {}".format(self.types))
 
         # Processing this into a DataFrame takes some time, so we cache the
         # generated frame in a CSV file
@@ -108,20 +120,37 @@ class ExperimentFrame:
 
             logging.debug("No cached DataFrame found, processing .dat files")
 
-            self.df = pd.DataFrame(data = {'op': [], 'lang': [], 'proc_count': [], 'measurement-num': [], 'measurement': []})
+            self.df = pd.DataFrame(data = {'op': [], 'lang': [], 'proc_count': [], 'type': [], 'measurement-num': [], 'measurement': []})
 
             i = 0
 
             for cnt in sorted(self.proc_counts):
                 for lang in sorted(self.langs):
                     for op in sorted(self.ops):
-                        # Note this assumes a specific file naming convention...
-                        logging.debug(f"Processing {lang}:{op}:{cnt}")
-                        with open(f"{self.dat_dir}/{op}_{lang}_{cnt}.dat", "r") as f:
-                            for j, line in enumerate(f.readlines()):
-                                row = pd.Series(data=[op, lang, cnt, j, int(line)], index=self.df.columns, name=i)
-                                self.df = self.df.append(row)
-                                i += 1
+                        for tp in sorted(self.types):
+                            # Note this assumes a specific file naming convention...
+                            logging.debug(f"Processing {lang}:{op}:{cnt}:{tp}")
+
+                            # Skip C + Opt combination, no such thing
+                            if tp == 'opt' and lang == "c":
+                                continue
+
+                            with open(f"{self.dat_dir}/{op}_{lang}_{cnt}_{tp}.dat", "r") as f:
+                                j = 0
+                                for line in f.readlines():
+
+                                    # this is dumb. it assumes there will only be *one* unique element coutn
+                                    # for all .dat files in the directory you pointed me to
+                                    if line[0] == '#':
+                                        a,b,c = line.split(',', maxsplit=2)
+                                        elms,val = b.split('=')
+                                        self.elms = int(val)
+                                        continue
+
+                                    row = pd.Series(data=[op, lang, cnt, tp, j, int(line)], index=self.df.columns, name=i)
+                                    self.df = self.df.append(row)
+                                    i += 1
+                                    j += 1
 
 
             self.df.to_hdf(f"{self.dat_dir}/{CACHEFILE}", key='df', mode='w')
@@ -137,11 +166,10 @@ class ExperimentFrame:
     # * https://gist.github.com/ctokheim/6435202a1a880cfecd71
     def plot_stacked(self, barwidth=0.25, of=None):
 
-        cm    = plt.get_cmap('nipy_spectral')
         f, ax = plt.subplots(1, figsize=(10,8))
 
         # Set up the color map 
-        ax.set_prop_cycle(cycler('color', [cm(1.*i/(2*len(self.ops))) for i in range(len(self.ops)*2)]))
+        colors = cm.inferno(np.linspace(0.1, 0.9, len(self.ops)))
 
         # we have one cluster of bars for each proc count
         bars  = self.proc_counts
@@ -150,38 +178,56 @@ class ExperimentFrame:
         # these should not be hard-coded, ideally passed in via command-line as list
         seriesa = {'comms': [], 'flops': [], 'reads': [], 'writes': []}
         seriesb = {'comms': [], 'flops': [], 'reads': [], 'writes': []}
+        seriesc = {'comms': [], 'flops': [], 'reads': [], 'writes': []}
 
         # bar X coords on the graph. The second set is just offset from the first bars by the bar width
         pos1 = np.arange(len(bars))
         pos2 = [x + barwidth for x in pos1]
+        pos3 = [x + barwidth for x in pos2]
 
         # These are the y coords for the bottoms of each component of a bar stack.
         # We'll update them as we go
         bottoma = np.zeros_like(bar_l).astype('float')
         bottomb = np.zeros_like(bar_l).astype('float')
+        bottomc = np.zeros_like(bar_l).astype('float')
 
         for c in self.proc_counts:
             for op in self.ops:
-                meana  = self.op_mean('c', c, op)
-                totala = self.exp_total('c', c)
+                meana  = self.op_mean('c', c, op, "unopt")
+                totala = self.exp_total('c', c, "unopt")
                 seriesa[op].append(meana/totala)
-                meanb  = self.op_mean('mpi', c, op)
-                totalb = self.exp_total('mpi', c)
+                meanb  = self.op_mean('julia', c, op, "unopt")
+                totalb = self.exp_total('julia', c, "unopt")
                 seriesb[op].append(meanb/totalb)
+                meanc  = self.op_mean('julia', c, op, "opt")
+                totalc = self.exp_total('julia', c, "opt")
+                seriesc[op].append(meanc/totalc)
 
-        for op in self.ops:
-            ax.bar(pos1, seriesa[op], bottom=bottoma, label=f"{op}-C", width=barwidth, edgecolor='white')
-            # "MPI" actually means "Julia" I believe
-            ax.bar(pos2, seriesb[op], bottom=bottomb, label=f"{op}-Julia", width=barwidth, edgecolor='white')
+
+        print(seriesa)
+        for i, op in enumerate(self.ops):
+            ax.bar(pos1, seriesa[op], bottom=bottoma, label=op, width=barwidth, edgecolor='white', color=colors[i])
+            ax.bar(pos2, seriesb[op], bottom=bottomb, label=op, width=barwidth, edgecolor='white', color=colors[i])
+            ax.bar(pos3, seriesc[op], bottom=bottomc, label=op, width=barwidth, edgecolor='white', color=colors[i])
             bottoma += seriesa[op]
             bottomb += seriesb[op]
+            bottomc += seriesc[op]
+
+
+        FONTSIZE=18
+
+        # Text on top of bars
+        [ax.text(p, 1, 'C', fontsize=FONTSIZE-1, rotation=30) for p in pos1]
+        [ax.text(p, 1, 'Julia', fontsize=FONTSIZE-1, rotation=30) for p in pos2]
+        [ax.text(p, 1, 'Julia (opt)', fontsize=FONTSIZE-1, rotation=30) for p in pos3]
 
         ax.set_xticks([r + barwidth for r in range(len(bars))])
-        ax.set_xticklabels(self.proc_counts, rotation=45, size='medium')
-        ax.set_xlabel("Total MPI Ranks", fontsize=12)
-        ax.set_ylabel("Execution Time Breakdown", fontsize=12)
+        ax.set_xticklabels(self.proc_counts, rotation=45, size=FONTSIZE)
+        ax.set_xlabel("Total MPI Ranks", fontsize=FONTSIZE)
+        ax.set_ylabel("Execution Time Breakdown", fontsize=FONTSIZE)
 
-        ax.legend(loc="best", bbox_to_anchor=(1,1), ncol=1, fontsize='medium')
+
+        ax.legend(loc="best", fontsize=FONTSIZE)
         #f.subplots_adjust(right=0.5, bottom=0.4)
 
         if of is None:
